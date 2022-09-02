eb9cb7682abdb6b9015e2a0ba3b74e15e322f951
Fabian Soriano <fabo.soriano@gmail.com>
2022-09-01 18:40:26 -0700

    able to combine suit and number and push into deck, now to find a function to do iterate for all values

--- index.js
+++ index.js
@@ -23,6 +23,20 @@
 
 
 
+// Define variable I will need to create a deck and use with functions.
+let deck = []
+
+// Images will not be applied yet so need to create arrays of card numbers and matching suits.
+// Find a way to use iterate with for loops
+let suits = ["C", "D", "H", "S"]  // clubs, diamonds, hearts, spades.
+let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+
+// Merging arrays will only create an array of A-K and C,D,H,S.  
+// Need to assign numbers to each suit and push that into the deck to create a playable deck.
+
+deck.push(suits[0] + numbers[0])
+console.log(deck)
+
 
 // Once you have cards, how to account for keeping them 21 and under or for the AI winning if they have 21 and under, but above yours.
 
