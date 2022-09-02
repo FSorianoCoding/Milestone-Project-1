263b26b580a222cbc297f095aa049c81433208d4
Fabian Soriano <fabo.soriano@gmail.com>
2022-09-01 20:44:03 -0700

    found a Durstenfeld shuffle alrogrithm that shuffles arrays

--- index.js
+++ index.js
@@ -8,27 +8,30 @@ let deck = []
 // Images will not be applied yet so need to create arrays of card numbers and matching suits.
 
 
-// Need to assign numbers to each suit and push that into the deck to create a playable deck.
 function createDeck() {
-    deck = [];   // was not working with "let deck = []"
+    deck = [];   
 
     let suits = ["C", "D", "H", "S"]  
-    // clubs, diamonds, hearts, spades.
-    
     let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    // forgot to add "10", was only getting 48 values initially.
-
+    
     for (let i=0; i < numbers.length; i++) {
-        // everywhere tried to have me concat to combine the arrays instead of adding the values of each array.
+    
         for (let s=0; s < suits.length; s++) {
             deck.push(numbers[i] + suits[s])
-            }
+        }
+    
     }
     return deck
 }
-// console.log(deck)
-// Array now returning combined values for a "deck of 52"
 
+// how to randomize array?
+function shuffleDeck() {
+    // Found the Durstenfeld shuffle to shuffle an array
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 // Once you have cards, how to account for keeping them 21 and under or for the AI winning if they have 21 and under, but above yours.
 
@@ -41,11 +44,17 @@ window.addEventListener("load", event => {
 });
 
 
+function test () {
+    console.log("TEST")
+}
 
 window.onload = function () {
-    createDeck()
+    createDeck();
+    test()
 }
 
 // works when outside of window.onload, but not inside.
 // createDeck()
 console.log(deck)
+
+// test() works but createDeck() doesn't inside of window.onload
